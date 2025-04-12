cecaf99bc475a63ce874dd331cb0ae6ad5c51d0f
liliana_toiunda <liliana.ntw@gmail.com>
2025-04-12 19:47:33 +0300

    changed index.html

--- js/task-3.js
+++ js/task-3.js
@@ -17,6 +17,27 @@
 
 // Залиш цей код для перевірки ментором.
 
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
