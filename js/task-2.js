7b6c94cc0cf5dd2f5c91ce1ac49b9dd5cdfe383d
liliana_toiunda <liliana.ntw@gmail.com>
2025-04-12 16:13:31 +0300

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -15,6 +15,24 @@
 // У консоль будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
 
 // Залиш цей код для перевірки ментором.
+
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
